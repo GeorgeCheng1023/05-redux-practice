3bc9cd741279b363dbc0863e3bad28da27c1d42f
GeorgeCheng1023 <ck40292@gmail.com>
2022-05-03 00:38:26 +0800

    feat: add userprofile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
-
+import {useDispatch, useSelector} from 'react-redux'
+import UserProfile from './components/UserProfile';
 function App() {
+  const dispatch = useDispatch()
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
   return (
     <>
     <Header/>
-    <Auth/>  
+    {isAuthenticated ? <UserProfile/> : <Auth/>}
+
     <Counter />
     </>
   );
