2a1cf71bfef410912d3379d5a5e6b808e359eb12
GeorgeCheng1023 <ck40292@gmail.com>
2022-05-03 00:24:59 +0800

    feat: auth reducer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,32 @@
 import classes from './Header.module.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {authActions} from '../store/index'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+    console.log(isAuthenticated)
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
+        {isAuthenticated && <ul>
           <li>
             <a href='/'>My Products</a>
           </li>
           <li>
             <a href='/'>My Sales</a>
           </li>
+          
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
-        </ul>
+        </ul>}
       </nav>
     </header>
   );
